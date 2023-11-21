55a9107ffc71b43b2512861dc43648adcb004475
sainath-1999 <sainathd1999@gmail.com>
2023-11-21 07:14:38 +0530

    the app is added with API call and shimmer effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,43 @@
 import resList from "../utils/mockData";
 import RestautrantCard from "./RestautrantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
 const Body = () => {
   // * local state variable --- Super powerful variable
-  const [reastaurentList, setReastaurentList] = useState(resList);
+  const [reastaurentList, setReastaurentList] = useState([]);
+
+  useEffect(() => {
+    fetcheData([]);
+  }, []);
+
+  const fetcheData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0825943&lng=80.2707234&restaurantId=37972&catalog_qa=undefined&submitAction=ENTER"
+    );
+
+    const json = await data.json();
+    console.log("json", json);
+    setReastaurentList(
+      json.data.cards[2].groupedCard?.cardGroupMap.REGULAR.cards[5].card.card
+        .itemCards
+    );
+  };
+
+  if (reastaurentList.length === 0) {
+    return (
+      <div>
+        <Shimmer />
+      </div>
+    );
+  }
   return (
     <div className="body">
       <div
         className="filter-btn"
         onClick={() => {
           const filteredlist = reastaurentList.filter(
-            (res) => res.info.avgRating > 4
+            (res) => res.card.info.ratings.aggregatedRating.rating > 4
           );
           setReastaurentList(filteredlist);
         }}
@@ -19,8 +46,8 @@ const Body = () => {
       </div>
       <div className="res-container">
         {/* <RestautrantCard resData={resList[0]} /> */}
-        {reastaurentList.map((restaurant) => (
-          <RestautrantCard key={restaurant.info.id} resData={restaurant} />
+        {reastaurentList?.map((restaurant, index) => (
+          <RestautrantCard key={index} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const MOCK_URL =
 
 export const LOGO_URL =
   "https://s.tmimgcdn.com/scr/800x500/325900/map-food-logo-design-template_325984-original.jpg";
+
+// https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf
