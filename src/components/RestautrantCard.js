c0de7c49a57a40aaa6f6640affb16df8b0802e78
sainath-1999 <sainathd1999@gmail.com>
2023-11-27 18:20:55 +0530

    Added styling using tailwind

--- src/components/RestautrantCard.js
+++ src/components/RestautrantCard.js
@@ -5,8 +5,14 @@ const RestautrantCard = (props) => {
     resData?.info;
   // console.log("props", props);
   return (
-    <div className="m-4 p-4 w-[250px] bg-gray-100 hover:bg-slate-300 rounded-lg">
-      <img className="rounded-lg" src={MOCK_URL + cloudinaryImageId} />
+    <div
+      className="m-4 p-4 w-[250px] bg-gray-100 hover:bg-slate-300 rounded-lg text-ellipsis
+     overflow-hidden whitespace-nowrap h-100"
+    >
+      <img
+        className="rounded-lg h-24 max-w-2xl"
+        src={MOCK_URL + cloudinaryImageId}
+      />
       <h3 className="font-bold py-2 text-lg">Name:{name}</h3>
       <h4>Price:{costForTwo}</h4>
       <h4>{cuisines.join(",")}</h4>
